999a8aa5a4b668fb947d8fb47a1d52a70fd175d8
SvetlanaMelekhova <71405460+SvetlanaMelekhova@users.noreply.github.com>
2021-01-26 11:57:51 +0300

    completed

---
+++ homework6_1_1/src/main/java/by/htp/first/homework6_1/AddCarActivity.kt
@@ -0,0 +1,109 @@
+package by.htp.first.homework6_1
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import by.htp.first.homework6_1.database.CarDatabase
+import by.htp.first.homework6_1.database.CarDatabaseDAO
+import by.htp.first.homework6_1.entity.Car
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.util.UUID
+
+private const val AUTHORITY: String = "by.htp.first.homework6_1.fileprovider"
+
+class AddCarActivity : AppCompatActivity() {
+
+    private lateinit var carPhoto: ImageView
+    private lateinit var carOwnerName: EditText
+    private lateinit var carProducerName: EditText
+    private lateinit var carModelName: EditText
+    private lateinit var carPlateNumber: EditText
+    private lateinit var buttonBack: ImageButton
+    private lateinit var buttonAdd: ImageButton
+    private lateinit var photoCamera: ImageButton
+    private lateinit var dao: CarDatabaseDAO
+    private var photoFile: File? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_car)
+        val toolbar: Toolbar = findViewById(R.id.toolBarActivityAddCar)
+        setSupportActionBar(toolbar)
+
+        dao = CarDatabase.getDataBase(this).getCarDatabaseDAO()
+        carPhoto = findViewById(R.id.backgroundActivityAddCar)
+        carOwnerName = findViewById(R.id.etOwnerNameActivityAdd)
+        carProducerName = findViewById(R.id.etProducerActivityAdd)
+        carModelName = findViewById(R.id.etModelActivityAdd)
+        carPlateNumber = findViewById(R.id.etPlateNumberActivityAdd)
+        buttonBack = findViewById(R.id.backButtonActivityAddCar)
+        buttonAdd = findViewById(R.id.addButtonActivityAddCar)
+        photoCamera = findViewById(R.id.photoActionButtonActivityAddCar)
+        photoCamera.setColorFilter(getColor(R.color.white))
+
+        buttonBack.setOnClickListener { finish() }
+
+        buttonAdd.setOnClickListener {
+            if (carOwnerName.text.isNotEmpty() && carModelName.text.isNotEmpty() && carPlateNumber.text.isNotEmpty()) {
+                val car = createCarObject()
+                dao.addCarToDatabase(car)
+                val intent = Intent().putExtra("objectId", car.id)
+                setResult(RESULT_OK, intent)
+                finish()
+            } else {
+                Snackbar.make(buttonAdd, getString(R.string.fill_out_form), Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        photoCamera.setOnClickListener {
+            photoFile = File(this.filesDir, UUID.randomUUID().toString() + " .jpg")
+            val photoUri = FileProvider.getUriForFile(this, AUTHORITY, photoFile!!)
+            val intentGetPhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intentGetPhoto.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            getFileWritingPermission(intentGetPhoto, photoUri)
+            startActivityForResult(intentGetPhoto, 5)
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun getFileWritingPermission(intentGetPhoto: Intent, photoUri: Uri?) {
+        val cameraActivities: List<ResolveInfo> =
+            packageManager.queryIntentActivities(intentGetPhoto, PackageManager.MATCH_DEFAULT_ONLY)
+        cameraActivities.forEach { cameraActivity ->
+            this.grantUriPermission(
+                cameraActivity.activityInfo.packageName,
+                photoUri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            )
+        }
+    }
+
+    private fun createCarObject() =
+        Car(
+            carOwnerName.text.toString(),
+            carProducerName.text.toString(),
+            carModelName.text.toString(),
+            carPlateNumber.text.toString(),
+            photoFile?.path
+        )
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        carPhoto.setImageURI(photoFile?.path?.toUri())
+    }
+}
+
+
---
+++ homework6_1_1/src/main/java/by/htp/first/homework6_1/EditCarActivity.kt
@@ -0,0 +1,145 @@
+package by.htp.first.homework6_1
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import by.htp.first.homework6_1.database.CarDatabase
+import by.htp.first.homework6_1.database.CarDatabaseDAO
+import by.htp.first.homework6_1.entity.Car
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.util.UUID
+
+private const val AUTHORITY: String = "by.htp.first.homework6_1.fileprovider"
+
+class EditCarActivity : AppCompatActivity() {
+
+    private var carId: Long = 0
+    private var photoFile: File? = null
+    private var photoUri: Uri? = null
+    private var car: Car? = null
+    private lateinit var dao: CarDatabaseDAO
+    private lateinit var imageBackground: ImageView
+    private lateinit var carOwnerName: EditText
+    private lateinit var carProducerName: EditText
+    private lateinit var carModelName: EditText
+    private lateinit var carPlateNumber: EditText
+    private lateinit var buttonBack: ImageView
+    private lateinit var buttonAdd: ImageView
+    private lateinit var photoCamera: ImageView
+    private lateinit var noPhotoTextView: TextView
+    private val positionCarInDatabase = "carId"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_car)
+        val toolbar: Toolbar = findViewById(R.id.toolBarActivityEditCar)
+        setSupportActionBar(toolbar)
+        dao = CarDatabase.getDataBase(this).getCarDatabaseDAO()
+        carOwnerName = findViewById(R.id.etOwnerNameActivityEdit)
+        carProducerName = findViewById(R.id.etProducerActivityEdit)
+        carModelName = findViewById(R.id.etModelActivityEdit)
+        carPlateNumber = findViewById(R.id.etPlateNumberActivityEdit)
+        buttonBack = findViewById(R.id.backButtonActivityEditCar)
+        buttonAdd = findViewById(R.id.addButtonActivityEditCar)
+        photoCamera = findViewById(R.id.photoActionButtonActivityEditCar)
+        imageBackground = findViewById(R.id.backgroundActivityEditCar)
+        noPhotoTextView = findViewById(R.id.tvNoPhotoActivityEditCar)
+        photoCamera.setColorFilter(getColor(R.color.white))
+
+        car = getCarData()
+
+        if (car != null) { fillPage() }
+
+        createFileAndUri()
+
+        buttonBack.setOnClickListener { finish() }
+
+        photoCamera.setOnClickListener {
+            val intentGetPhoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE).putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            getPermissions(intentGetPhoto)
+            startActivityForResult(intentGetPhoto, 1)
+        }
+
+        buttonAdd.setOnClickListener { addListener() }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun getPermissions(intentGetPhoto: Intent) {
+        val cameraActivities: List<ResolveInfo> = packageManager.queryIntentActivities(intentGetPhoto, PackageManager.MATCH_DEFAULT_ONLY)
+        cameraActivities.forEach { cameraActivity ->
+            this.grantUriPermission(
+                    cameraActivity.activityInfo.packageName,
+                    photoUri,
+                    Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+        }
+    }
+
+    private fun createFileAndUri() {
+        photoFile = File(this.filesDir, UUID.randomUUID().toString() + " .jpg")
+        photoUri = FileProvider.getUriForFile(this, AUTHORITY, photoFile!!)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imageBackground.setImageURI(photoFile?.path?.toUri())
+        noPhotoTextView.visibility = View.INVISIBLE
+    }
+
+    private fun addListener() {
+        if (isAllTextAvailable()) {
+            val car = fillCarObject()
+            dao.update(car)
+            Intent().apply {
+                putExtra(positionCarInDatabase, carId)
+                setResult(RESULT_OK, this)
+                finish()
+            }
+        } else {
+            Snackbar.make(buttonAdd, getString(R.string.fill_out_form), Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun fillCarObject() =
+            Car(carOwnerName.text.toString(),
+                    carProducerName.text.toString(),
+                    carModelName.text.toString(),
+                    carPlateNumber.text.toString(),
+                    photoUri.toString()).also { it.id = carId }
+
+    private fun getCarData(): Car {
+        carId = intent.getLongExtra(positionCarInDatabase, 0)
+        return dao.getCar(carId)
+    }
+
+    private fun fillPage() {
+        car?.let {
+            carOwnerName.setText(it.carOwnerName)
+            carProducerName.setText(it.carProducerName)
+            carModelName.setText(it.carModelName)
+            carPlateNumber.setText(it.carPlateNumber)
+        }
+        if (car?.carImage == null) {
+            imageBackground.setImageResource(R.drawable.ic_background_view)
+        } else {
+            imageBackground.setImageURI(car?.carImage?.toUri())
+            noPhotoTextView.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun isAllTextAvailable() =
+            carOwnerName.text.isNotEmpty() && carProducerName.text.isNotEmpty() && carModelName.text.isNotEmpty() && carPlateNumber.text.isNotEmpty()
+}
+
